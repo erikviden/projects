d67be977574868da8b165c3152feac6182544b0d
Erik Viden <splat9709@gmail.com>
2024-04-20 12:37:52 +0200

    Homework

--- homework2.py
+++ homework2.py
@@ -3,14 +3,7 @@ account_balance = 0
 operations = []
 
 print("Commands:")
-print("- balance\n"
-        "- sale\n"
-          "- purchase\n"
-          "- account\n"
-          "- list\n"
-          "- warehouse\n"
-          "- review\n"
-          "- end\n")
+print("- balance\n""- sale\n""- purchase\n""- account\n""- list\n""- warehouse\n""- review\n""- end\n")
 
 while True:
     command = input("Type a command to be performed:")
@@ -21,16 +14,17 @@ while True:
         if balance == "add":
             add = float(input("Enter the amount you want to add to your account balance:"))
             account_balance += add
-            account_balance = round(account_balance,2)
-            print(f"You have successfully added {add} $ to your account balance, your account balance is now {account_balance} $.\n")
+            account_balance = round(account_balance, 2)
+            print(f"You have successfully added {add} $ to your account balance."
+                  f"Your account balance is now {account_balance} $.\n")
             operations.append(f"Added {add} $ to account balance.")
-
 
         elif balance == "subtract":
             subtract = float(input("Enter the amount you want to subtract from your account balance:"))
             if (account_balance-subtract) > 0:
-                account_balance = round(account_balance,2)
-                print(f"You have successfully subtracted {subtract} $ from your account balance, your account balance is now {account_balance} $.\n")
+                account_balance = round(account_balance, 2)
+                print(f"You have successfully subtracted {subtract} $ from your account balance."
+                      f"Your account balance is now {account_balance} $.\n")
                 operations.append(f"Subtracted {subtract} $ from account balance.")
             if (account_balance-subtract) < 0:
                 print(f"Insufficient balance.Your account balance is {account_balance}.")
@@ -45,7 +39,7 @@ while True:
         product = input("Enter product name:")
         price = int(input("Enter price per unit:"))
         quantity = int(input("Enter units sold:"))
-        price = round(price,2)
+        price = round(price, 2)
         if product in warehouse and warehouse[product]["quantity"] >= quantity:
             total_price = price * quantity
             warehouse[product]["quantity"] -= quantity
@@ -64,7 +58,7 @@ while True:
             if product in warehouse:
                 warehouse[product]["quantity"] += quantity
             else:
-                warehouse[product] = {"price":price, "quantity":quantity}
+                warehouse[product] = {"price": price, "quantity": quantity}
             print(f"Purchased {quantity} {product} for {total_price}$")
             operations.append(f"{quantity} units of {product} purchased for {total_price} $.")
 
@@ -76,7 +70,7 @@ while True:
             print("Product not available at the moment.")
 
     elif command == "list":
-        for product,details in warehouse.items():
+        for product, details in warehouse.items():
             print(f"{product}: {details['quantity']} units - ${details['price']} per unit")
 
     elif command == "review":
